732b9f8a4ddac0a2c120a7cace94425c7e81d9be
Sergej Salmin <several989@mail.ru>
2019-09-23 21:31:26 +0600

    Допиливаю
    Добавил элементы, тестовые классы, подключил статические данные к визуальным элементам.

---
+++ library/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostSpamer.library
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ library/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostSpamer.library
+{
+    public static class TestClass
+    {
+        public static List<Server> Servers { get; } = new List<Server>
+        {
+            new Server { Id = 0, Name = "Yandex", Adress = "smtp.yandex.ru", UserName = "UserName", Password = "Pass" },
+            new Server { Id = 1, Name = "Google", Adress = "smtp.google.com", UserName = "UserName", Password = "Pass" },
+            new Server { Id = 2, Name = "Mail.ru", Adress = "smtp.mail.ru", UserName = "UserName", Password = "Pass" },
+            new Server { Id = 3, Name = "Sobaka.com", Adress = "smtp.sobaka.com", UserName = "UserName", Password = "Pass" }
+        };
+
+        public static List<Sender> Senders { get; } = new List<Sender>
+        {
+            new Sender { Id = 0, Name = "Карпатов Тролл", Address = "zabugor@gulp.su" },
+            new Sender { Id = 1, Name = "Кроликов Иван", Address = "ivan.krolikov@meil.tru" },
+            new Sender { Id = 2, Name = "Вездеколов Фарид", Address = "kudayberdy@coil.co"}
+        };
+    }
+}
